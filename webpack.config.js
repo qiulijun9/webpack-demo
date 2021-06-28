879b94e801c365a6e5c341b2341310cef366f68c
qiulijun <qiulijun@zcool.com.cn>
2021-06-28 10:20:54 +0800

    fix: clear  code

--- webpack.config.js
+++ webpack.config.js
@@ -1,103 +1,121 @@
-const {
-  resolve
-} = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const webpack = require("webpack");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const {
-  CleanWebpackPlugin
-} = require("clean-webpack-plugin");
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   //入口配置
-  entry: "./src/index.js",
+  // entry: './src/other.js',
   //多入口配置：
-  // entry: {
-  //  index:"./src/index.js",
-  // other:"./src/other.js"
-
-  //}
+  entry: {
+    index: './src/index.js',
+    other: './src/other.js',
+  },
   //出口配置
   output: {
-    filename: "bundle.js",
-    path: resolve(__dirname, "dist") //必须是绝对路径
+    filename: 'bundle.js',
+    path: resolve(__dirname, 'dist'), //必须是绝对路径
+  },
+  // 切分chunk
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          chunk: 'initial',
+          minChunks: 2,
+          minSize: 0,
+        },
+      },
+      vendor: {
+        test: /node_modules/,
+        chunk: 'initial',
+        name: 'vendor',
+        enforce: true,
+      },
+    },
   },
   //loader
   module: {
-    rules: [{
+    rules: [
+      {
         test: /.html$/,
-        use: "html-withimg-loader" //在html中引入img
+        use: 'html-withimg-loader', //在html中引入img
       },
       {
         test: /\.css$/,
         // loader: ["style-loader", "css-loader"] //css loader
         loader: ExtractTextPlugin.extract({
           //把css打包到指定文件夹
-          fallback: "style-loader",
-          use: ["css-loader", "postcss-loader"], //执行顺序，从后往前postcss-loader 兼容浏览器， css-loader变成css ,style-loader插入head中
-          publicPath: "../" //css中文件的路径
-        })
+          fallback: 'style-loader',
+          use: ['css-loader', 'postcss-loader'], //执行顺序，从后往前postcss-loader 兼容浏览器， css-loader变成css ,style-loader插入head中
+          publicPath: '../', //css中文件的路径
+        }),
       },
       {
         test: /\.less$/,
-        use: ["style-loader", "css-loader", "less-loader "] //less loader
+        use: ['style-loader', 'css-loader', 'less-loader '], //less loader
       },
       {
         test: /\.(sass|scss)$/,
-        use: ["style-loader", "css-loader", "sass-loader "] //sass loader
+        use: ['style-loader', 'css-loader', 'sass-loader '], //sass loader
       },
       {
         test: /\.(jpg|jpeg|png|gif|svg|webp)$/,
-        use: [{
-          loader: "url-loader",
-          options: {
-            limit: 4 * 1024, //超过4k，转换成base64的图片
-            outputPath: "imgs", //打包后输出的路径
-            esModule: false
-          }
-        }]
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 4 * 1024, //超过4k，转换成base64的图片
+              outputPath: 'imgs', //打包后输出的路径
+              esModule: false,
+            },
+          },
+        ],
       },
       {
         test: /\.(jsx|js)$/,
         use: {
-          loader: "babel-loader?cacheDirectory",
+          loader: 'babel-loader?cacheDirectory',
           options: {
-            presets: ["@babel/preset-env"]
-          }
+            presets: ['@babel/preset-env'],
+          },
         },
-        exclude: /node_modules/
-      }
+        exclude: /node_modules/,
+      },
     ],
-    noParse: [/react\.min\.js$/]
+    noParse: [/react\.min\.js$/],
   },
   //插件
   plugins: [
     new webpack.ProvidePlugin({
       //全局引入第三方库
-      React: "react",
-      Component: ["react", "Component"]
+      React: 'react',
+      Component: ['react', 'Component'],
     }),
     new CleanWebpackPlugin(), //每次打包前清空dist目录
     new webpack.HotModuleReplacementPlugin(), //开启热更新
     //生成html模板
     new HtmlWebpackPlugin({
-      template: "./public/index.html", //定义模板
-      title: "webpack", //定义html title
+      template: './public/index.html', //定义模板
+      title: 'webpack', //定义html title
       hash: true, //缓存,
-      filename: "index.html" //打包后的文件名
+      filename: 'index.html', //打包后的文件名
     }),
-    new ExtractTextPlugin("css/index.css") //打包css文件到指定文件夹和js 文件分离
+    new ExtractTextPlugin('css/index.css'), //打包css文件到指定文件夹和js 文件分离
   ],
   //开发服务器
   devServer: {
-    contentBase: resolve(__dirname, "build"), //服务器访问的地址
+    contentBase: resolve(__dirname, 'build'), //服务器访问的地址
     compress: true, // 启动 gzip
-    host: "localhost", //ip
+    host: 'localhost', //ip
     port: 3000,
     open: true, // 自动打开浏览器
-    hot: true //热更新
+    hot: true, //热更新
   },
   //模式 开发环境 development，生产环境 production
-  mode: "development"
-  // devtool: "cheap-module-eval-source-map" //开发环境下使用
+  mode: 'development',
+  // devtool: 'cheap-module-eval-source-map', //开发环境下使用
+  // devtool: 'source-map', 开启source-map 会生成一个chunk 和两个bundle
 };
